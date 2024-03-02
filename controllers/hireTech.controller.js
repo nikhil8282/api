9b89bca68129b18d41d91ff9cf94cfaa7127900a
Nikhil Singh <singhnikhil8282@gmail.com>
2024-03-02 17:55:17 +0530

    techStatus update api

--- controllers/hireTech.controller.js
+++ controllers/hireTech.controller.js
@@ -1,4 +1,4 @@
-const hireTech = require("../models/hireTech");
+const HireTech = require("../models/hireTech");
 // const ResourceManagement = require("../models/resourceManagement");
 
 const getJobsBySkill = async (req, res) => {
@@ -7,9 +7,8 @@ const getJobsBySkill = async (req, res) => {
   try {
     if (!skill) return res.status(400).json("Bad request");
 
-    let result = await hireTech.find({
-
-      selectedSkills: {$all:skill}
+    let result = await HireTech.find({
+      selectedSkills: { $elemMatch: { $in: skill } },
     });
     // console.log(result);
     return res.json(result);
@@ -19,6 +18,66 @@ const getJobsBySkill = async (req, res) => {
 
   return res.status(400).json(err.message);
 };
+const getJobs = async (req, res) => {
+
+  try {
+
+    let result = await HireTech.find();
+    // console.log(result);
+    return res.json(result);
+  } catch (err) {
+    return res.status(400).json(err.message);
+  }
+
+  return res.status(400).json(err.message);
+};
+
+
+const setJobData = async (req, res) => {
+  const { jobTitle, noPositions, experience, selectedSkills } = req.body;
+  try {
+    if (!jobTitle || !noPositions || !experience || !selectedSkills)
+      throw new Error("All fields required");
+    let result = await HireTech.create({
+      jobTitle,
+      noPositions,
+      experience,
+      selectedSkills,
+    });
+    return res.json(result);
+  } catch (err) {
+    return res.status(400).json(err.message);
+  }
+};
+
+const updateTechStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status, date } = req.body;
+  // console.log(`${weekStartDate}  ${weekEndDate}  ${taskDate}`);
+  console.log(id,status,date);
+  try {
+    if (!id || !status || !date) return res.status(400).json("Bad request");
+
+    let result = await HireTech.updateOne(
+      { _id: id },
+      {
+        $addToSet: {
+          techStatus: {"status":status,"date":date}
+        }
+      }
+    );
+    console.log(result);
+    return res.json(result);
+  } catch (err) {
+    return res.status(400).json(err.message);
+  }
+
+  return res.status(400).json(err.message);
+};
+
 module.exports = {
   getJobsBySkill,
+  updateTechStatus,
+  setJobData,
+  getJobs
 };
--- routes/hireTech.routes.js
+++ routes/hireTech.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { getJobsBySkill } = require("../controllers/hireTech.controller");
+const { getJobsBySkill,updateTechStatus,setJobData, getJobs } = require("../controllers/hireTech.controller");
 const router = express.Router();
 router.get("/jobs/", getJobsBySkill);
-// router.put("/tasks/tasksDescription",updateTaskdes);
+router.get("/allJobs/", getJobs);
+router.post("/jobs/",setJobData);
+router.put("/jobs/update/techStatus/:id",updateTechStatus);
 module.exports = router;
